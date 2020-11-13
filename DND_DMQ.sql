741d32fcbd6e525e6fd59cbf0a3b417a36087630
Bhavin <bhavin.shahj@gmail.com>
2020-11-12 23:30:59 -0800

    forgot query stub

---
+++ DND_DMQ.sql
@@ -0,0 +1,49 @@
+-- Bhavin Shah and Jeovani Velas
+-- DND Char Manager
+--11/12/2020
+
+-- For all variable name the colon : will be used to 
+-- denoate the front-end variable name being used
+
+-- Query for adding new character
+
+-- Query for adding new Statistics
+
+-- Query for adding new inventory
+
+-- Query for adding new ability
+
+-- Query for selecting statistics based statID
+
+-- Query for selecting inventory based on inventoryID
+
+-- Query for selecting class based on classID
+
+-- Query for searching inventory items by Name
+
+-- Query for select character by Name
+
+-- Query for classes by name
+
+-- Query for action by name
+
+-- Query for updating char entity
+-- This does not update statistic, or inventory
+
+-- Query for updating inventory
+
+-- Query for updating Ability
+
+-- Quer for updating classes
+
+-- Query for deleting Char by CharID
+
+-- Query for deleting class by classID
+
+-- Query for deleting statistic by statID
+
+-- Query for deleting inventory by CharID
+
+-- Query for deleting action by actionID
+
+
